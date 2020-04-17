6387015ed371252ffedb5e920e012c3d232ffbe4
Vitalc <1053794412@qq.com>
2020-04-17 18:01:18 +0800

    123

--- SpringProject/test/src/Hello.java
+++ SpringProject/test/src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
 
+        System.out.println("111");
+
         System.out.println("222");
     }
 }
